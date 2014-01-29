25f418aaafa2ef46e106cd70758dc4abfd04299c
Garrett Honeycutt <code@garretthoneycutt.com>
2014-01-29 18:27:34 -0500

    initial import

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,42 @@
-# modules/aide/manifests/init.pp - manage aide
-# Copyright (C) 2007 admin@immerda.ch
-# 
-
+# == Class: aide
+#
+# Full description of class aide here.
+#
+#
 class aide {
-    case $operatingsystem {
-        gentoo: { include aide::gentoo }
-        default: { include aide::base }
+
+    package { 'aide':
+        ensure => present,
+        notify => Exec['build-aide-db'],
+    }
+
+    # Do the initial build of the aide DB
+    exec { 'build-aide-db':
+        refreshonly => true,
+        command => '/usr/sbin/aide --init',
+        timeout => 0,
+        notify => Exec['make-aide-db-live'],
+    }
+
+    # Make the aide live for comparison later after initial build
+    exec { 'make-aide-db-live':
+        refreshonly => true,
+        command => '/bin/cp /var/lib/aide/aide.db.new.gz /var/lib/aide/aide.db.gz',
+    }
+
+    file { "/etc/aide.conf":
+        source => "puppet:///modules/aide/aide.conf",
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+        mode => 0644,
+    }
+
+    file { "/etc/cron.daily/aide.cron":
+        source => "puppet:///modules/aide/aide.cron",
+        ensure => file,
+        owner => 'root',
+        group => 'root',
+        mode => 0755,
     }
 }
